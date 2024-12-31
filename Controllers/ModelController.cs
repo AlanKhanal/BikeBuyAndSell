39599a22b0c6090421d6f2ed880e8655b6e70a1c
Alan Khanal <alankhanal2001@gmail.com>
2025-01-01 03:27:06 +0545

    Added Vehicle Model -C18

---
+++ Controllers/ModelController.cs
@@ -0,0 +1,47 @@
+﻿using MashWebApplication.AppDbContext;
+using MashWebApplication.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace MashWebApplication.Controllers
+{
+    public class ModelController : Controller
+    {
+        private readonly MashDbContext _db;
+
+        [BindProperty]
+        public ModelViewModel ModelVM { get; set; }
+
+        public ModelController(MashDbContext db)
+        {
+            _db= db;
+            ModelVM = new ModelViewModel() 
+            {
+                Makes = _db.Makes.ToList(),
+                Model=new Models.Model() 
+            };
+
+        }
+
+        public IActionResult Index()
+        {
+            var model = _db.Models.Include(m=>m.Make);
+            return View(model);
+        }
+        public IActionResult Create()
+        {
+            return View(ModelVM);
+        }
+        [HttpPost,ActionName("Create")]
+        public IActionResult CreatePost()
+        {
+            if (ModelState.IsValid)
+            {
+                return View (ModelVM);
+            }
+            _db.Models.Add(ModelVM.Model);
+            _db.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
